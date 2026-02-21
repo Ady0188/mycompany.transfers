392f2d573dbb5afc6f719bf4db7db51daf4f7bcb
Adab Saidmirzoev <adab0188@gmail.com>
2026-02-21 05:49:28 +0500

    Latest changes

--- Clean/MyCompany.Transfers.Admin/MyCompany.Transfers.Admin.Client/Program.cs
+++ Clean/MyCompany.Transfers.Admin/MyCompany.Transfers.Admin.Client/Program.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using MyCompany.Transfers.Admin.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddMudServices();
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<AuthHeaderHandler>();
+builder.Services.AddScoped<IAgentsApiService, AgentsApiService>();
+builder.Services.AddScoped<IProvidersApiService, ProvidersApiService>();
 
 await builder.Build().RunAsync();
